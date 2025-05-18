2bf8d615f98fd0f37fd7c71311f7bad90558e57a
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-05-18 15:36:16 +0300

    implemented api functions

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,40 @@
 import './HomePage.module.css';
+import { useState, useEffect } from 'react';
+import Loader from '../../components/Loader/Loader';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+import { getTrandingMovies } from '../../api/movies';
 
 const HomePage = () => {
+  const [hits, setHits] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const tranding_movies_fetching = async () => {
+      try {
+        setIsLoading(true);
+
+        const data = await getTrandingMovies(1);
+
+        if (data && data.results) {
+          setHits(data.results);
+          setError(false);
+        } else {
+          setError(true);
+        }
+      } catch (error) {
+        console.error(error);
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    tranding_movies_fetching();
+  }, []);
+
+  console.log('hits', hits);
+
   return <div>HomePage</div>;
 };
 
